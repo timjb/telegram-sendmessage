098182bdf228b41eeae8a5c946ce67fb20d39052
kmein <kieran.meinhardt@gmail.com>
2016-08-04 20:57:01 +0200

    Update stack resolver in telegram.hs

--- telegram.hs
+++ telegram.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env stack
--- stack --resolver lts-3.8 --install-ghc runghc --package wreq --package directory
+-- stack --resolver lts-6.8 --install-ghc runghc --package wreq --package directory
 
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
